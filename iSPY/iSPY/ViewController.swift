3a7cad5db8875a61ee2fcc71bbe64d6c0c2e56da
amrish036 <amrish.036@gmail.com>
2018-08-15 22:08:54 +1000

    Initial Commit for iSPY

---
+++ iSPY/iSPY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSPY
+//
+//  Created by Amrish Mahesh on 15/8/18.
+//  Copyright © 2018 Amrish Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
