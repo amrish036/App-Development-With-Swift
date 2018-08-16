bffcfc12fe2555354d212c004666b0846dc7a024
amrish036 <amrish.036@gmail.com>
2018-08-16 20:37:33 +1000

    Add Meal and Food Models

---
+++ Table Views/MealTracker/MealTracker/Food.swift
@@ -0,0 +1,14 @@
+//
+//  Food.swift
+//  MealTracker
+//
+//  Created by Amrish Mahesh on 16/8/18.
+//  Copyright © 2018 Amrish Mahesh. All rights reserved.
+//
+
+import Foundation
+
+struct Food {
+  var name: String
+  var description: String
+}
---
+++ Table Views/MealTracker/MealTracker/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  MealTracker
+//
+//  Created by Amrish Mahesh on 16/8/18.
+//  Copyright © 2018 Amrish Mahesh. All rights reserved.
+//
+
+import Foundation
+
+struct Meal {
+  var name: String
+  var food: [Food]
+}
